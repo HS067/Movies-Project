7cc4c4a00fedc967ae539693f734d5231771116d
HS067 <harsimranpal2001@gmail.com>
2021-06-16 12:54:42 +1200

    Add project files.

---
+++ Movies Project/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Movies_Project;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //defining an object with in the class Database from MovieStore
+        DatabaseManager Obj_Test = new DatabaseManager();
+        [TestMethod]
+        public void TestDBConection()
+        {
+            // Variable of actual and expected connection String 
+            var ActualDBCon = Obj_Test.str;
+            var ExpexctedDBCon = "Data Source=DESKTOP-J8NDLG6\\SQLEXPRESS01;Initial Catalog=mydata;Integrated Security=True";
+            //Assert - checking the output is which expected
+            Assert.AreEqual(ExpexctedDBCon, ActualDBCon);
+        }
+    }
+}
